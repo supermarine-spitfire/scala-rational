eb081fda27a0cf589c853f7a3377a3c4d4ac83c1
imanuel <imanuel@icubecsi.com>
2018-12-26 13:11:08 -0500

    Defined an auxiliary constructor. This constructor allows for any user to define a Rational of the form a/1 as simply a, where a is an integer.

--- Rational/src/main/scala/Rational.scala
+++ Rational/src/main/scala/Rational.scala
@@ -2,9 +2,14 @@ package main.scala
 
 class Rational(n: Int, d: Int){
   require(d != 0)
+
   val numer: Int = n
   val denom: Int = d
+
+  def this(n: Int) = this(n, 1) // auxiliary constructor
+
   override def toString: String = n + "/" + d
+
   def add(that: Rational): Rational =
     new Rational(
       numer * that.denom + that.numer * denom,
